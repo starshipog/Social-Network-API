82ce8322810f9b148a77b5b0fc8d48c7c790cd5b
starshipog <chayewhitaker14@gmail.com>
2023-10-29 12:07:11 -0500

    updated code

---
+++ Develop/models/User.js
@@ -0,0 +1,31 @@
+const { Schema, Types } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    userName: {
+      type: String,
+      required: true,
+    },
+    score: {
+      type: Number,
+      required: true,
+      default: () => Math.floor(Math.random() * (100 - 70 + 1) + 70),
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = userSchema;
